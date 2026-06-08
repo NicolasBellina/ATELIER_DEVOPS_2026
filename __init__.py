28716b8f9aa6d50065214d4cd4c485bf3cc35f48
Nicolas Bellina <nicolasbellina72@gmail.com>
2026-06-08 08:46:23 +0000

    Add quality gate for exercise 2

--- __init__.py
+++ __init__.py
@@ -1,15 +1,4 @@
-from flask import Flask
-from flask import render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return "<h2>Bonjour tout le monde !</h2><p>Pour accéder à vos exerices cliquez <a href='./exercices/'>Ici</a></p>"
-
-@app.route('/exercices/')
-def exercices():
-    return render_template('exercices.html')
+from app import app
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=False)
